5e92506af16147e368ca916b7019c115496923e9
savithareddy26 <savithakandi26@gmail.com>
2023-06-05 19:15:06 -0400

    stu

---
+++ student.java
@@ -0,0 +1,12 @@
+package employeee;
+
+public class student {
+	public String studname;
+	public int studid;
+	public int studage;
+	public float studgpa;
+	public String studbranch;
+	public static void main(String[] args) {
+		System.out.println("Hello this is an student portal");
+	}
+}
