049a3ad81eff9b97be2c14ea3115c9ffa590867c
savithareddy26 <savithakandi26@gmail.com>
2023-06-05 19:14:39 -0400

    person

---
+++ person.java
@@ -0,0 +1,13 @@
+package employeee;
+
+public class person {
+	public String pername;
+	public String perdob;
+	public String peraddress;
+	public int peridproof;
+	public static void main(String[] args) {
+		System.out.println("Hello this is an person portal");
+	}
+	}
+
+
